755df9ab2396473de1af4bd54e64a59db0433ef6
pissalou <pascal.mazars@gmail.com>
2025-08-25 20:48:33 +0200

    print arraybuffer as hex from worker.js

--- concurrency/worker.js
+++ concurrency/worker.js
@@ -1,7 +1,15 @@
 self.onmessage = async function(e) {
   const receivedData = e.data;
-  console.log(`Worker: received ${JSON.stringify(e.data)}`);
-  await new Promise(resolve => setTimeout(resolve, 5000)); // Sleep for 5 seconds
+  if (e.data instanceof ArrayBuffer) {
+        const uint8Array = new Uint8Array(e.data);
+        const hexString = Array.from(uint8Array)
+            .map(byte => byte.toString(16).padStart(2, '0'))
+            .join(' ');
+        console.log('Worker: received binary (ArrayBuffer) as hex ', hexString);
+  } else {
+    console.log(`Worker: received ${JSON.stringify(e.data)}`);
+  }
+  await new Promise(resolve => setTimeout(resolve, 2000)); // Sleep for 2 seconds
   // TODO: perform computations
   self.postMessage({ result: "Computation complete!" });
 };
